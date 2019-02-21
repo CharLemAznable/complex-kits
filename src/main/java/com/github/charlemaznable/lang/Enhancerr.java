636fe34bab7b12581dd6fbef37fb1f65a63999af
CharLemAznable <545541819@qq.com>
2019-02-21 11:09:33 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/lang/Enhancerr.java
+++ src/main/java/com/github/charlemaznable/lang/Enhancerr.java
@@ -1,5 +1,6 @@
 package com.github.charlemaznable.lang;
 
+import lombok.val;
 import net.sf.cglib.proxy.Callback;
 import net.sf.cglib.proxy.CallbackFilter;
 import net.sf.cglib.proxy.Enhancer;
@@ -8,7 +9,7 @@ public class Enhancerr {
 
     public static Object create(Class type, Callback callback,
                                 Class[] argumentTypes, Object[] arguments) {
-        Enhancer e = new Enhancer();
+        val e = new Enhancer();
         e.setSuperclass(type);
         e.setCallback(callback);
         return e.create(argumentTypes, arguments);
@@ -16,7 +17,7 @@ public class Enhancerr {
 
     public static Object create(Class superclass, Class[] interfaces, Callback callback,
                                 Class[] argumentTypes, Object[] arguments) {
-        Enhancer e = new Enhancer();
+        val e = new Enhancer();
         e.setSuperclass(superclass);
         e.setInterfaces(interfaces);
         e.setCallback(callback);
@@ -25,7 +26,7 @@ public class Enhancerr {
 
     public static Object create(Class superclass, Class[] interfaces, CallbackFilter filter, Callback[] callbacks,
                                 Class[] argumentTypes, Object[] arguments) {
-        Enhancer e = new Enhancer();
+        val e = new Enhancer();
         e.setSuperclass(superclass);
         e.setInterfaces(interfaces);
         e.setCallbackFilter(filter);
