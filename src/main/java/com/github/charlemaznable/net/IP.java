f357682967a1c89ffc21e870ba6e5b340040376c
CharLemAznable <545541819@qq.com>
2019-05-14 11:03:05 +0800

    Add IP.V4/IP.V6 ip getter.

--- src/main/java/com/github/charlemaznable/net/IP.java
+++ src/main/java/com/github/charlemaznable/net/IP.java
@@ -3,21 +3,40 @@ package com.github.charlemaznable.net;
 import lombok.SneakyThrows;
 import lombok.val;
 
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+
 import static java.net.NetworkInterface.getNetworkInterfaces;
 
-public class IP {
+public enum IP {
+
+    V4{
+        @Override
+        protected boolean checkInetAddress(InetAddress inetAddress) {
+            return inetAddress instanceof Inet4Address;
+        }
+    },
+    V6{
+        @Override
+        protected boolean checkInetAddress(InetAddress inetAddress) {
+            return inetAddress instanceof Inet6Address;
+        }
+    };
+
+    protected abstract boolean checkInetAddress(InetAddress inetAddress);
 
     @SneakyThrows
-    public static String localIP() {
+    public String localIP() {
         val netInterfaces = getNetworkInterfaces();
         while (netInterfaces.hasMoreElements()) {
             val ni = netInterfaces.nextElement();
             val address = ni.getInetAddresses();
             while (address.hasMoreElements()) {
                 val ip = address.nextElement();
-                if (ip.isSiteLocalAddress()
-                        && !ip.isLoopbackAddress()
-                        && !ip.getHostAddress().contains(":")) {
+                if (ip.isSiteLocalAddress() &&
+                        !isReservedAddress(ip) &&
+                        checkInetAddress(ip)) {
                     return ip.getHostAddress();
                 }
             }
@@ -26,20 +45,26 @@ public class IP {
     }
 
     @SneakyThrows
-    public static String netIP() {
+    public String netIP() {
         val netInterfaces = getNetworkInterfaces();
         while (netInterfaces.hasMoreElements()) {
             val ni = netInterfaces.nextElement();
             val address = ni.getInetAddresses();
             while (address.hasMoreElements()) {
                 val ip = address.nextElement();
-                if (!ip.isSiteLocalAddress()
-                        && !ip.isLoopbackAddress()
-                        && !ip.getHostAddress().contains(":")) {
+                if (!ip.isSiteLocalAddress() &&
+                        !isReservedAddress(ip) &&
+                        checkInetAddress(ip)) {
                     return ip.getHostAddress();
                 }
             }
         }
         return localIP();
+    }
+
+    private boolean isReservedAddress(InetAddress inetAddress) {
+        return (inetAddress.isLinkLocalAddress() ||
+                inetAddress.isLoopbackAddress() ||
+                inetAddress.isAnyLocalAddress());
     }
 }
--- src/test/java/com/github/charlemaznable/net/IPTest.java
+++ src/test/java/com/github/charlemaznable/net/IPTest.java
@@ -3,17 +3,29 @@ package com.github.charlemaznable.net;
 import lombok.var;
 import org.junit.jupiter.api.Test;
 
-import static com.github.charlemaznable.net.IP.localIP;
-import static com.github.charlemaznable.net.IP.netIP;
+import static com.github.charlemaznable.net.IP.V4;
+import static com.github.charlemaznable.net.IP.V6;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class IPTest {
 
     @Test
-    public void testIP() {
-        var ip = netIP();
+    public void testIPV4() {
+        var ip = V4.netIP();
+        System.out.println(ip);
         assertNotEquals("127.0.0.1", ip);
-        ip = localIP();
+        ip = V4.localIP();
+        System.out.println(ip);
         assertNotEquals("127.0.0.1", ip);
+    }
+
+    @Test
+    public void testIPV6() {
+        var ip = V6.netIP();
+        System.out.println(ip);
+        assertNotEquals("0:0:0:0:0:0:0:1", ip);
+        ip = V6.localIP();
+        System.out.println(ip);
+        assertNotEquals("0:0:0:0:0:0:0:1", ip);
     }
 }
