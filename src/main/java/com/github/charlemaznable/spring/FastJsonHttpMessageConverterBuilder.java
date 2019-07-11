f5493b1acf99abf0637ef3576fc1c4adad334d7a
CharLemAznable <545541819@qq.com>
2019-07-11 16:08:43 +0800

    Modify FastJsonHttpMessageConverterBuilder.

--- src/main/java/com/github/charlemaznable/spring/FastJsonHttpMessageConverterBuilder.java
+++ src/main/java/com/github/charlemaznable/spring/FastJsonHttpMessageConverterBuilder.java
@@ -1,10 +1,18 @@
 package com.github.charlemaznable.spring;
 
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.parser.deserializer.ParseProcess;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeFilter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import lombok.val;
+import org.springframework.http.MediaType;
 
 import java.nio.charset.Charset;
+import java.util.Map;
 
 import static com.github.charlemaznable.lang.Listt.newArrayList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
@@ -18,15 +26,59 @@ public class FastJsonHttpMessageConverterBuilder {
         return this;
     }
 
+    public FastJsonHttpMessageConverterBuilder setSerializeConfig(SerializeConfig serializeConfig) {
+        this.fastJsonConfig.setSerializeConfig(serializeConfig);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setParserConfig(ParserConfig parserConfig) {
+        this.fastJsonConfig.setParserConfig(parserConfig);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setParseProcess(ParseProcess parseProcess) {
+        this.fastJsonConfig.setParseProcess(parseProcess);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setSerializerFeatures(SerializerFeature... serializerFeatures) {
+        this.fastJsonConfig.setSerializerFeatures(serializerFeatures);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setSerializeFilters(SerializeFilter... serializeFilters) {
+        this.fastJsonConfig.setSerializeFilters(serializeFilters);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setFeatures(Feature... features) {
+        this.fastJsonConfig.setFeatures(features);
+        return this;
+    }
+
+    public FastJsonHttpMessageConverterBuilder setClassSerializeFilters(Map<Class<?>, SerializeFilter> classSerializeFilters) {
+        this.fastJsonConfig.setClassSerializeFilters(classSerializeFilters);
+        return this;
+    }
+
     public FastJsonHttpMessageConverterBuilder dateFormat(String dateFormat) {
         this.fastJsonConfig.setDateFormat(dateFormat);
         return this;
     }
 
+    public FastJsonHttpMessageConverterBuilder setWriteContentLength(boolean writeContentLength) {
+        this.fastJsonConfig.setWriteContentLength(writeContentLength);
+        return this;
+    }
+
     public FastJsonHttpMessageConverter build() {
+        return build(APPLICATION_JSON_UTF8);
+    }
+
+    public FastJsonHttpMessageConverter build(MediaType... mediaTypes) {
         val fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
         fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-        fastJsonHttpMessageConverter.setSupportedMediaTypes(newArrayList(APPLICATION_JSON_UTF8));
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(newArrayList(mediaTypes));
         return fastJsonHttpMessageConverter;
     }
 }
