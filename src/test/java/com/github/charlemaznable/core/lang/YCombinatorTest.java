458e07d8c05feafff0b10b1bb941c1cd9101de7b
CharLemAznable <545541819@qq.com>
2019-09-18 02:45:52 +0800

    Upgrade, fixed test coverage.

--- src/test/java/com/github/charlemaznable/core/lang/YCombinatorTest.java
+++ src/test/java/com/github/charlemaznable/core/lang/YCombinatorTest.java
@@ -8,14 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class YCombinatorTest {
 
     @Test
-    public void testFac() {
+    public void testYCombinator() {
+        new YCombinator();
+
         val fac = YCombinator.<Integer, Integer>of(
                 recFunc -> n -> n < 2 ? 1 : (n * recFunc.apply(n - 1)));
         assertEquals(3628800, fac.apply(10));
-    }
 
-    @Test
-    public void testFib() {
         val fib = YCombinator.<Integer, Integer>of(
                 recFunc -> n -> n <= 2 ? 1 : (recFunc.apply(n - 1) + recFunc.apply(n - 2)));
         assertEquals(55, fib.apply(10));
