664c1daa6808859dd9b8532185109103b941d4e7
CharLemAznable <545541819@qq.com>
2019-09-24 11:16:10 +0800

    Upgrade, add cacheable y-combinator.

--- src/main/java/com/github/charlemaznable/core/lang/YCombinator.java
+++ src/main/java/com/github/charlemaznable/core/lang/YCombinator.java
@@ -1,5 +1,7 @@
 package com.github.charlemaznable.core.lang;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.val;
 
 import java.util.function.Function;
@@ -16,5 +18,37 @@ public class YCombinator {
             return r.apply(r);
         };
         return g.apply(b);
+    }
+
+    public static <T, R> Function<T, R> of(CacheableFunction<T, R> c) {
+        Function<Function<Function<T, R>, Function<T, R>>, Function<T, R>> g = f -> {
+            Function<Function, Function<T, R>> r = p -> {
+                @SuppressWarnings("unchecked")
+                val w = (Function<Function, Function<T, R>>) p;
+                return f.apply(param -> {
+                    R present = c.getIfPresent(param);
+                    if (null != present) return present;
+                    R result = w.apply(w).apply(param);
+                    return c.put(param, result);
+                });
+            };
+            return r.apply(r);
+        };
+        return g.apply(c);
+    }
+
+    public static abstract class CacheableFunction<T, R>
+            implements Function<Function<T, R>, Function<T, R>> {
+
+        private Cache<T, R> cache = CacheBuilder.newBuilder().build();
+
+        public R getIfPresent(T key) {
+            return cache.getIfPresent(key);
+        }
+
+        public R put(T key, R value) {
+            cache.put(key, value);
+            return value;
+        }
     }
 }
--- src/test/java/com/github/charlemaznable/core/lang/YCombinatorTest.java
+++ src/test/java/com/github/charlemaznable/core/lang/YCombinatorTest.java
@@ -1,8 +1,13 @@
 package com.github.charlemaznable.core.lang;
 
+import com.github.charlemaznable.core.lang.YCombinator.CacheableFunction;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.function.Function;
+
+import static com.github.charlemaznable.core.lang.Listt.newArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YCombinatorTest {
@@ -11,12 +16,67 @@ public class YCombinatorTest {
     public void testYCombinator() {
         new YCombinator();
 
+        List<Integer> facOrder = newArrayList();
         val fac = YCombinator.<Integer, Integer>of(
-                recFunc -> n -> n < 2 ? 1 : (n * recFunc.apply(n - 1)));
+                recFunc -> n -> {
+                    facOrder.add(n);
+                    return n < 2 ? 1 : (n * recFunc.apply(n - 1));
+                });
+        facOrder.clear();
         assertEquals(3628800, fac.apply(10));
+        assertEquals(10, facOrder.size());
+        facOrder.clear();
+        assertEquals(3628800, fac.apply(10));
+        assertEquals(10, facOrder.size());
 
+        List<Integer> fibOrder = newArrayList();
         val fib = YCombinator.<Integer, Integer>of(
-                recFunc -> n -> n <= 2 ? 1 : (recFunc.apply(n - 1) + recFunc.apply(n - 2)));
+                recFunc -> n -> {
+                    fibOrder.add(n);
+                    return n <= 2 ? 1 : (recFunc.apply(n - 1) + recFunc.apply(n - 2));
+                });
+        fibOrder.clear();
         assertEquals(55, fib.apply(10));
+        assertEquals(109, fibOrder.size());
+        fibOrder.clear();
+        assertEquals(55, fib.apply(10));
+        assertEquals(109, fibOrder.size());
+    }
+
+    @Test
+    public void testYCombinatorCache() {
+        List<Integer> facOrder = newArrayList();
+        val fac = YCombinator.of(new CacheableFunction<Integer, Integer>() {
+            @Override
+            public Function<Integer, Integer> apply(Function<Integer, Integer> recFunc) {
+                return n -> {
+                    facOrder.add(n);
+                    return n < 2 ? 1 : (n * recFunc.apply(n - 1));
+                };
+            }
+        });
+        facOrder.clear();
+        assertEquals(3628800, fac.apply(10));
+        assertEquals(10, facOrder.size());
+        facOrder.clear();
+        assertEquals(3628800, fac.apply(10));
+        assertEquals(1, facOrder.size());
+
+        List<Integer> fibOrder = newArrayList();
+        val fib = YCombinator.of(new CacheableFunction<Integer, Integer>() {
+            @Override
+            public Function<Integer, Integer> apply(Function<Integer, Integer> recFunc) {
+                return n -> {
+                    fibOrder.add(n);
+                    return n <= 2 ? 1 : (recFunc.apply(n - 1) + recFunc.apply(n - 2));
+                };
+            }
+        });
+        fibOrder.clear();
+        assertEquals(55, fib.apply(10));
+        assertEquals(10, fibOrder.size());
+        fibOrder.clear();
+        assertEquals(55, fib.apply(10));
+        assertEquals(1, fibOrder.size());
     }
 }
