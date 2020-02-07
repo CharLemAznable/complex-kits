09437e379c03c119ddf0c1311c3cff58196345fa
CharLemAznable <545541819@qq.com>
2020-02-07 20:33:59 +0800

    Fixed test case.

--- src/test/java/com/github/charlemaznable/core/net/ohclient/testscan/TestComponent.java
+++ src/test/java/com/github/charlemaznable/core/net/ohclient/testscan/TestComponent.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestComponent {
 
-    @Autowired
+    @Autowired(required = false)
     public TestHttpClient testHttpClient;
 }
