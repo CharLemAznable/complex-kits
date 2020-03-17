c21f48767f02d1007c9f2f38d8af9cba0ec0679f
CharLemAznable <545541819@qq.com>
2020-03-17 17:25:40 +0800

    Some refactor.

--- src/main/java/com/github/charlemaznable/core/net/ohclient/internal/OhConstant.java
+++ src/main/java/com/github/charlemaznable/core/net/ohclient/internal/OhConstant.java
@@ -3,6 +3,7 @@ package com.github.charlemaznable.core.net.ohclient.internal;
 import com.github.charlemaznable.core.net.common.ContentFormat.ContentFormatter;
 import com.github.charlemaznable.core.net.common.ContentFormat.FormContentFormatter;
 import com.github.charlemaznable.core.net.common.HttpMethod;
+import com.google.common.net.HttpHeaders;
 
 import java.nio.charset.Charset;
 
@@ -11,9 +12,9 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class OhConstant {
 
-    public static final String ACCEPT_CHARSET = "Accept-Charset";
+    public static final String ACCEPT_CHARSET = HttpHeaders.ACCEPT_CHARSET;
     public static final Charset DEFAULT_ACCEPT_CHARSET = UTF_8;
-    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String CONTENT_TYPE = HttpHeaders.CONTENT_TYPE;
     public static final ContentFormatter DEFAULT_CONTENT_FORMATTER = new FormContentFormatter();
     public static final HttpMethod DEFAULT_HTTP_METHOD = GET;
     public static final long DEFAULT_CALL_TIMEOUT = 0;
