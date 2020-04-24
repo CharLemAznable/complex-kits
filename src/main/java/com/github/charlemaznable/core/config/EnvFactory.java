2d0e563e9af041584f88f591f78c243265a52f09
CharLemAznable <545541819@qq.com>
2020-04-24 19:37:56 +0800

    Fixed code smell.

--- src/main/java/com/github/charlemaznable/core/config/EnvFactory.java
+++ src/main/java/com/github/charlemaznable/core/config/EnvFactory.java
@@ -60,10 +60,6 @@ public final class EnvFactory {
         return LoadingCachee.get(envLoaderCache, factory);
     }
 
-    private static String substitute(String source) {
-        return envClassPathSubstitutor.replace(source);
-    }
-
     @SuppressWarnings("unchecked")
     public static class EnvLoader {
 
@@ -170,6 +166,10 @@ public final class EnvFactory {
             String defaultValue = DefaultValueProvider.class == providerClass ? envConfig.defaultValue()
                     : FactoryContext.apply(factory, providerClass, p -> p.defaultValue(envClass, method));
             return substitute(blankThen(defaultValue, () -> null));
+        }
+
+        private String substitute(String source) {
+            return envClassPathSubstitutor.replace(source);
         }
 
         private Object parseValue(String key, String value, Method method) {
